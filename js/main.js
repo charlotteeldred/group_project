b1d0ed78d071074b991af5d42cc6336be42b75e3
charlotteeldred <55197280+charlotteeldred@users.noreply.github.com>
2020-01-04 19:42:11 +0000

    added tabs for other types of cheese options

--- js/main.js
+++ js/main.js
@@ -3,12 +3,20 @@ console.log('Linked')
 // Only fires logic after the page has fully loaded
 $( document ).ready(function(){
 
-  $(".option").click(function(){
+  $(".option1").click(function(){
     $(this).addClass('border');
-    $(".option").not(this).removeClass('border');
+    $(".option1").not(this).removeClass('border');
     });
 
+  $(".option2").click(function(){
+    $(this).addClass('border');
+    $(".option2").not(this).removeClass('border');
+    });
 
+  $(".option3").click(function(){
+    $(this).addClass('border');
+    $(".option3").not(this).removeClass('border');
+    });
 
 
 
