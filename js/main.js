4274270b48363cf405002ecbc96317d98b3841ff
charlotteeldred <55197280+charlotteeldred@users.noreply.github.com>
2020-01-06 13:56:23 +0000

    Added unselect option on build page

--- js/main.js
+++ js/main.js
@@ -4,27 +4,48 @@ console.log('Linked')
 $( document ).ready(function(){
 
   $(".option1").click(function(){
-    $(this).addClass('border');
-    $(".option1").not(this).removeClass('border');
-    $(".selection1.text").addClass('hidden');
-    $(".selection1.cheesepic").hide();
-    $(".selection1.cheesepic."+this.id).show();
+    if ( $(this).hasClass('border') ) {
+      $(this).removeClass('border');
+      $(".selection1.text").removeClass('hidden');
+      $(".selection1.cheesepic."+this.id).hide();
+    }
+    else {
+      $(this).addClass('border');
+      $(".option1").not(this).removeClass('border');
+      $(".selection1.text").addClass('hidden');
+      $(".selection1.cheesepic").hide();
+      $(".selection1.cheesepic."+this.id).show();
+    }
     });
 
   $(".option2").click(function(){
-    $(this).addClass('border');
-    $(".option2").not(this).removeClass('border');
-    $(".selection2.text").addClass('hidden');
-    $(".selection2.cheesepic").hide();
-    $(".selection2.cheesepic."+this.id).show();
+    if ( $(this).hasClass('border') ) {
+      $(this).removeClass('border');
+      $(".selection2.text").removeClass('hidden');
+      $(".selection2.cheesepic."+this.id).hide();
+    }
+    else {
+      $(this).addClass('border');
+      $(".option2").not(this).removeClass('border');
+      $(".selection2.text").addClass('hidden');
+      $(".selection2.cheesepic").hide();
+      $(".selection2.cheesepic."+this.id).show();
+    }
     });
 
   $(".option3").click(function(){
-    $(this).addClass('border');
-    $(".option3").not(this).removeClass('border');
-    $(".selection3.text").addClass('hidden');
-    $(".selection3.cheesepic").hide();
-    $(".selection3.cheesepic."+this.id).show();
+    if ( $(this).hasClass('border') ) {
+      $(this).removeClass('border');
+      $(".selection3.text").removeClass('hidden');
+      $(".selection3.cheesepic."+this.id).hide();
+    }
+    else {
+      $(this).addClass('border');
+      $(".option3").not(this).removeClass('border');
+      $(".selection3.text").addClass('hidden');
+      $(".selection3.cheesepic").hide();
+      $(".selection3.cheesepic."+this.id).show();
+    }
     });
 
 });
